8518814e905e9969b9a418f73c25870bdc162393
Nvdepart <67013480+Nvdepart@users.noreply.github.com>
2022-12-20 18:06:52 +0100

    landing

---
+++ codi/layouts/default.vue
@@ -0,0 +1,7 @@
+<template>
+  <v-app style="background-color:beige">
+    <v-main>
+     <nuxt />
+    </v-main>
+  </v-app>
+</template>
---
+++ codi/pages/index.vue
@@ -0,0 +1,11 @@
+<template>
+  <v-container style="height:100vh">
+    <v-row align="center" justify="center" style="height:100%">
+      <v-col align="center">
+        <div style="font-size:100;font-size:40px;color:brown">
+          <h1>Hello</h1>
+        </div>
+      </v-col>
+    </v-row>
+  </v-container>
+</template>
